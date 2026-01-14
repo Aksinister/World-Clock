2e33723d99dcb2577ddd546c72c42ee5532f407a
Aksinister <91866119+Aksinister@users.noreply.github.com>
2026-01-14 11:38:08 +1000

    Added real time date and time functionality
    I plugged in moment.js into my code and used it to format and then display the time and date of the listed countries.
    I used a setInterval() function to update the time every second.
    
    I also removed some the fake html date and time from the code.

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,38 @@
-//alert("hello");
+function updateRomeTime() {
+  //rome
+  let romeElement = document.querySelector("#rome");
+  let romeDate = romeElement.querySelector(".date");
+  let romeTime = romeElement.querySelector(".time");
+  let romeMoment = moment().tz("Europe/Rome");
+
+  romeDate.innerHTML = romeMoment.format("MMMM Do YYYY ");
+  romeTime.innerHTML = romeMoment.format("h:mm:ss[<small>]A[</small>]");
+}
+
+function updateLisbonTime() {
+  //Lisbon
+  let lisbonElement = document.querySelector("#lisbon");
+  let lisbonDate = lisbonElement.querySelector(".date");
+  let lisbonTime = lisbonElement.querySelector(".time");
+  let lisbonMoment = moment().tz("Europe/Lisbon");
+
+  lisbonDate.innerHTML = lisbonMoment.format("MMMM Do YYYY ");
+  lisbonTime.innerHTML = lisbonMoment.format("h:mm:ss[<small>]A[</small>]");
+}
+
+function updateBrisbaneTime() {
+  //brisbane
+  let brisbaneElement = document.querySelector("#brisbane");
+  let brisbaneDate = brisbaneElement.querySelector(".date");
+  let brisbaneTime = brisbaneElement.querySelector(".time");
+  let brisbaneMoment = moment().tz("Australia/Brisbane");
+
+  brisbaneDate.innerHTML = brisbaneMoment.format("MMMM Do YYYY ");
+  brisbaneTime.innerHTML = brisbaneMoment.format("h:mm:ss[<small>]A[</small>]");
+}
+
+setInterval(() => {
+  updateRomeTime();
+  updateLisbonTime();
+  updateBrisbaneTime();
+}, 1000);
